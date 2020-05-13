8cbe82d2426a19aeaec6ad27473c73ba2cab937d
David Tekan <david.tekan@sap.com>
2020-05-13 10:13:16 -0700

    anyam fozte edition

--- alpine/mariadb/create-database.sh
+++ alpine/mariadb/create-database.sh
@@ -12,7 +12,7 @@ else
 
 	chown -R mysql:mysql /var/lib/mysql
 
-	mysql_install_db --user=mysql > /dev/null
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
 		MYSQL_ROOT_PASSWORD=`pwgen 16 1`
